990c4989918af135fb41ccdcfa09ad883494c1dd
sudipeb <sudip.koirala@ebpearls.com>
2025-09-18 17:10:32 +0545

    minor changes between the action button

--- lib/main.dart
+++ lib/main.dart
@@ -21,17 +21,32 @@ class _MyAppState extends State<MyApp> {
           leading: IconButton(icon: Icon(Icons.arrow_back), onPressed: () { },),
           title: const Text("Weather App"),
           centerTitle: true,
-          backgroundColor: Colors.purple,
+          backgroundColor: Colors.white,
           actions: [IconButton(icon: Icon(Icons.settings), onPressed: () { },),
         ]),
         body: SafeArea(
-          child: const Center(
-            child: Text("Hello World", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.red) ),
-          ),
+          child: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [Colors.blueAccent, Colors.pink]
+              )
+            ),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Welcome to Flutter", style: TextStyle(fontSize: 24, color: Colors.white, fontWeight: FontWeight.bold),),
+                  SizedBox(height: 20,),
+                  ElevatedButton(onPressed: () { }, child: Text("Get Started"), ),
+                ],
+              ),
+            ),
         ),
       ),
       
-    );
+    ));  
     
   }
 }
