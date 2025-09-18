af2929e87bda7543acedf73967555c835482b48d
sudipeb <sudip.koirala@ebpearls.com>
2025-09-18 15:16:47 +0545

    demo app project initialized

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+ void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter demo app",
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        // backgroundColor: Colors.yellow,
+        appBar: AppBar(
+          title: const Text("Flutter demo app"),
+          centerTitle: true,
+          backgroundColor: Colors.purple,
+        ),
+        body: SafeArea(
+          child: const Center(
+            child: Text("Hello World", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.red) ),
+            
+          ),
+        ),
+      ),
+      
+    );
+    
+  }
+}
